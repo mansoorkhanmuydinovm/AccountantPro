20d04d11e68ca9b5003a831863eaaefd2614fac7
misterpozitiv1607 <mansurjonmoydinov16072002@gmail.com>
2024-11-30 17:31:54 +0500

    Domain/Entities/Clients/Client

---
+++ AccountantPro.Domain/Entities/Bases/Auditable.cs
@@ -0,0 +1,8 @@
+﻿namespace AccountantPro.Domain.Entities.Bases;
+
+public class Auditable
+{
+    public long Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+}
---
+++ AccountantPro.Domain/Entities/Clients/Client.cs
@@ -0,0 +1,17 @@
+﻿using AccountantPro.Domain.Entities.Bases;
+
+namespace AccountantPro.Domain.Entities.Clients;
+
+public class Client:Auditable
+{
+    public string FullName { get; set; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public string TelegramPhoneNumber { get; set; }
+    public string JSHSHIR { get; set; }
+    public string Bank { get; set; }
+    public string BankAddress { get; set; }
+    public string INN { get; set; }
+    public string OKONX { get; set; }
+
+}
